7bb238414b0351206dddb453140f4635e71751df
melike keskin <melike.keskin@tum.de>
2020-12-19 20:42:34 +0100

    chess board creater method added and sample usage showed

---
+++ ChessEquipment.py
@@ -0,0 +1,53 @@
+class Board:
+    def __init__(self):
+            self.board_length = 800
+    def DrawChessBoard(self, board_length = 800):
+        import pygame
+        import sys
+        from pygame.locals import QUIT
+
+        pygame.init()
+
+
+        # Assign FPS a value
+        fps = 30
+        FramePerSec = pygame.time.Clock()
+
+        # Setting up color objects
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+
+        # Setup a board_length x board_length pixel display with caption
+        ChessBoard = pygame.display.set_mode((board_length,board_length))
+        ChessBoard.fill(WHITE)
+        pygame.display.set_caption("CHESS")
+
+
+        # create board frames
+        square_position1 = 0
+        square_position2 = 0
+        square_length = board_length/8
+        count = 0
+        while square_position1 < (board_length):
+            square_position2 = 0
+            if count%2 == 0:
+                square_position2 = square_length
+            count = count + 1
+            while square_position2 < (board_length):
+                pygame.draw.rect(ChessBoard, BLACK, 
+                                 [square_position1, square_position2, square_length, square_length], 
+                                 border_radius=2,border_top_left_radius=-1, border_top_right_radius=-1,
+                                 border_bottom_left_radius=-1, border_bottom_right_radius=-1)
+                square_position2 = square_position2 + square_length*2
+            square_position1 = square_position1 + square_length
+
+
+        # Beginning Game Loop
+        while True:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            FramePerSec.tick(fps)
