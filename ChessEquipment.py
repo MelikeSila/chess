66bf2c21c58265e6ee9ef13297ff004d005621f1
melike keskin <melike.keskin@tum.de>
2020-12-20 00:10:14 +0100

    stable board created

--- ChessEquipment.py
+++ ChessEquipment.py
@@ -20,6 +20,7 @@ class Board:
         pygame.init()
 
         # Setting up color objects
+        Colour = (100,120,10)
         BLACK = (0, 0, 0)
         WHITE = (255, 255, 255)
 
@@ -34,19 +35,43 @@ class Board:
         square_position1 = square_length
         square_position2 = square_length
         count = 0
+        position_names = ["a","b","c","d","e","f","g","h"]
+        
+        ####################
+        # draw the frame of the board
+        pygame.draw.rect(ChessBoard, BLACK, 
+                                 [square_length, square_length, 
+                                  board_length - square_length, board_length - square_length], width = 4, 
+                                 border_radius=2,border_top_left_radius=-1, border_top_right_radius=-1,
+                                 border_bottom_left_radius=-1, border_bottom_right_radius=-1)
+        
         ########################################
-        ###TODO draw the square names such as b1 a2 etc
+        #draw the position name and the squares of the chessboard
         while square_position1 < (board_length):
+            from pygame import font
             square_position2 = square_length
             if count%2 == 0:
                 square_position2 = square_length*2
             count = count + 1
+            
+            ####################
+            ### draw the position name
+            font = pygame.font.SysFont("comicsansms", int(square_length/4))
+            text1 = font.render(str(count), True, BLACK)
+            text2 = font.render(position_names[count-1], True, BLACK)
+            ChessBoard.blit(text1,(0 + square_length/4 , square_position1  + square_length/4))
+            pygame.display.flip()
+            ChessBoard.blit(text2,(square_position1 + square_length/4 , 0  + square_length/4))
+            
+            ####################
+            ### draw the squares of the chessboard
             while square_position2 < (board_length):
                 pygame.draw.rect(ChessBoard, BLACK, 
                                  [square_position1, square_position2, square_length, square_length], 
                                  border_radius=2,border_top_left_radius=-1, border_top_right_radius=-1,
                                  border_bottom_left_radius=-1, border_bottom_right_radius=-1)
                 square_position2 = square_position2 + square_length*2
+            ####################
             square_position1 = square_position1 + square_length
         self.pygame = pygame
     
